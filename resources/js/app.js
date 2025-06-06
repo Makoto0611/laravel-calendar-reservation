17094cb3e4f5e327f3a41ce376737cfe3b0b384e
Makoto0611 <saruhashi0611@gmail.com>
2025-06-06 21:47:33 +0900

    setup bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
+
+// より実用的なBootstrapテスト
+document.body.innerHTML = `
+  <div class="container mt-5">
+    <div class="alert alert-success">
+      <h4 class="alert-heading">Bootstrap動作中！</h4>
+      <p>Bootstrap CSSが正常に適用されています。</p>
+    </div>
+    <button class="btn btn-primary">Bootstrapボタン</button>
+  </div>
+`;
